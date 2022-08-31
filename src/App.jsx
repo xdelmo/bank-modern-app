f41cbfa42104d71bcdf9df0a33ee1beeece3ef51
Emanuele <84925809+xdelmo@users.noreply.github.com>
2022-08-31 15:58:50 +0200

    FIX: warning value never used

--- src/App.jsx
+++ src/App.jsx
@@ -26,16 +26,16 @@ const App = () => {
     },
   };
 
-  const showChildrenVariants = {
-    hidden: { opacity: 0, scale: 0.5 },
-    show: {
-      opacity: 1,
-      scale: 1,
-      transition: {
-        duration: 1,
-      },
-    },
-  };
+  // const showChildrenVariants = {
+  //   hidden: { opacity: 0, scale: 0.5 },
+  //   show: {
+  //     opacity: 1,
+  //     scale: 1,
+  //     transition: {
+  //       duration: 1,
+  //     },
+  //   },
+  // };
 
   const slideRightVariants = {
     hidden: {
