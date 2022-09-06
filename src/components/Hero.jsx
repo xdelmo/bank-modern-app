16fa62b7f5b5cd5693a051043928ffba01ccda65
Emanuele <84925809+xdelmo@users.noreply.github.com>
2022-09-06 16:10:37 +0200

    FIX: discount div proportions

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,13 +14,19 @@ const Hero = ({ slideLeftVariants, slideRightVariants }) => (
       whileInView={slideRightVariants.whileInView}
       viewport={slideRightVariants.viewport}
     >
+      {/* DISCOUNT DIV */}
       <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-        <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
-        <p className={`${styles.paragraph}`}>
+        <img
+          src={discount}
+          alt="discount"
+          className="sm:w-[32px] sm:h-[32px] w-[52px] h-[52px] mr-2"
+        />
+        <p className={`${styles.paragraph} leading-snug`}>
           <span className="text-white">20% </span>
           Discount For <span className="text-white">1 Month </span>Account
         </p>
       </div>
+
       <div className="flex flex-row items-center justify-between w-full">
         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
           The Next <br className="hidden sm:block" />{" "}
